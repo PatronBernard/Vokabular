ef07847b198fdcd9ec371bea38c05cfcc543210c
PatronBernard <jan.morez@gmail.com>
2014-09-27 19:54:43 +0200

    Cleanup

--- ExerciseSession.py
+++ ExerciseSession.py
@@ -1,31 +1,35 @@
 import random,sys
-from openpyxl import load_workbook
+from openpyxl import load_workbook,exceptions
 from PyQt4 import QtCore
 
 class ExerciseSession:
-    dictionary  =[]   #Contains each word with its translation
-    score       =0         
-    ex_amount   =0
-    ex_no       =1
-    dataLoaded  =False
+    vocabularium= []   #Contains each word with its translation
+    score       = 0         
+    ex_amount   = 0
+    ex_no       = 1
+    dataLoaded  = False
     
     def __init__(self):
         pass
         
-    def LoadExercises(self,filename):
-        wb=load_workbook(filename)
-        ws=wb.active
-        for row in ws.rows:
-            self.dictionary.append([row[0].value,row[1].value])
-        for word in self.dictionary:
-            print(word)
+    def loadExercises(self,filename):
+        try: 
+            wb=load_workbook(filename)
+            ws=wb.active
+            for row in ws.rows:
+                self.vocabularium.append([row[0].value,row[1].value])
+            self.dataLoaded = True
+        except:
+            pass
+            #print('No such file found. Please restart the application.')
             
-        self.dataLoaded = True
+        
     def shuffleExercises(self):
         if self.dataLoaded:
-            random.shuffle(self.dictionary)
+            random.shuffle(self.vocabularium)
         else:
-            print('ShuffleExercises: Dictionary is empty!')
+            pass
+            #print('ShuffleExercises: vocabularium is empty!')
                 
     def getScore(self):
         return self.score
@@ -34,17 +38,17 @@ class ExerciseSession:
         self.score=0
         
     def getDictSize(self):
-        return len(self.dictionary)
+        return len(self.vocabularium)
         
     def getExercise(self):
-        print(self.ex_no)
+        #print(self.ex_no)
         if self.dataLoaded:
-            return str(self.dictionary[self.ex_no % len(self.dictionary)][0])
+            return str(self.vocabularium[self.ex_no % len(self.vocabularium)][0])
 
     def checkExercise(self,user_answer):
         if self.dataLoaded:
             self.ex_amount+=1
-            if cleanstr(user_answer)==cleanstr(self.dictionary[self.ex_no % len(self.dictionary)][1]):
+            if cleanstr(user_answer)==cleanstr(self.vocabularium[self.ex_no % len(self.vocabularium)][1]):
                 self.score+=1
                 return True  
             else:
@@ -52,11 +56,7 @@ class ExerciseSession:
                 
     
     def nextExercise(self):
-        #if self.ex_no < self.ex_amount-1:
             self.ex_no+=1
-        #else:
-            #self.ex_no=0
-            #self.resetScore()
             
 #Basic method that cleans and decodes strings for easy comparison
 def cleanstr(istring):
@@ -69,6 +69,3 @@ def cleanstr(istring):
         decoded=str(istring.toUtf8(),'utf-8')
         tempstr=decoded.lower()
         return tempstr.strip()
-    
-   
-    
--- Vertaal_OefeningQT.py
+++ Vertaal_OefeningQT.py
@@ -4,16 +4,19 @@ from PyQt4 import QtGui,QtCore
 
 #PyQT Interface 
 class MainWindow(QtGui.QWidget):
-    #Logic object
-    ExSess=ExerciseSession.ExerciseSession()
+    #This is the internal logic class
+
+    
     
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.ExSess=ExerciseSession.ExerciseSession()
         self.initUI()
-    
+  
+  
     def loadData(self,filename):
         if len(filename)!=0:
-            self.ExSess.LoadExercises(filename)
+            self.ExSess.loadExercises(filename)
             if self.ExSess.dataLoaded:
                 self.writeToLog('Successfully loaded ' + str(self.ExSess.getDictSize()) + ' words from \" ' + filename + ' \" ')
                 self.writeToLog('========================================')
@@ -21,55 +24,54 @@ class MainWindow(QtGui.QWidget):
                 self.showExercise()
             else:
                 self.writeToLog('Failed to load ' + '\"' + filename + '\"')
+                self.writeToLog('Please restart the application.')
         else:
             self.writeToLog('No command line input file supplied. Please restart the application.')
 
             
-                
-        
     def initUI(self):    
         #Main window
         self.setWindowTitle('Vocabulary Exercise') 
         self.setGeometry(300, 300, 400, 300)  
         
         #Log
-        #!!! WHY SELF? WHY HERE??!!!
         self.outputLog = QtGui.QTextEdit()
         self.outputLog.setReadOnly(True)
         
         #User Input Line
         self.lineEdit=QtGui.QLineEdit()
-        
-        
+         
         #Submit Button
-        sButton=QtGui.QPushButton()
-        sButton.setText('Submit')
-        sButton.setToolTip('Submit Answer (Enter)')
-        sButton.clicked.connect(self.sendToInputProcessor)
+        self.sButton=QtGui.QPushButton()
+        self.sButton.setText('Submit')
+        self.sButton.setToolTip('Submit Answer (Enter)')
+        self.sButton.clicked.connect(self.sendToInputProcessor)
         
         #VBox
-        vbox=QtGui.QVBoxLayout(self)
-        vbox.setGeometry(self.frameGeometry())
-        vbox.addWidget(self.outputLog)
-        vbox.addWidget(self.lineEdit)
-        vbox.addWidget(sButton)
+        self.vbox=QtGui.QVBoxLayout(self)
+        self.vbox.setGeometry(self.frameGeometry())
+        self.vbox.addWidget(self.outputLog)
+        self.vbox.addWidget(self.lineEdit)
+        self.vbox.addWidget(self.sButton)
         
         #Signal connections
-        
         self.lineEdit.setFocus()
         self.show()
         
+        
     def writeToLog(self,istring):
         self.outputLog.append(istring)
+        
         
     def getUserInput(self):
         user_input=self.lineEdit.text()
         return user_input
         
+        
     def showExercise(self):
         self.writeToLog(self.ExSess.getExercise())
         
-    #Rewrite this whole thing!
+        
     def sendToInputProcessor(self):   
         user_input=self.lineEdit.text()
         self.outputLog.append(user_input)
@@ -100,11 +102,11 @@ class MainWindow(QtGui.QWidget):
             self.sendToInputProcessor()
 
         
-        
+    
+    
 def main():
     
     app = QtGui.QApplication(sys.argv)
-    #Testing the ExerciseSession object
     MainW = MainWindow()
     if len(sys.argv)==2:
         MainW.loadData(sys.argv[1])
