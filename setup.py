4ed96c555dad7462c265af02513befdbce708503
PatronBernard <jan.morez@gmail.com>
2014-10-05 22:53:29 +0200

    Py2Exe

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+import py2exe
+
+#python setup.py py2exe --includes sip
+#You might have to do this too:
+#http://stackoverflow.com/questions/23970240/how-to-stop-python-program-compiled-in-py2exe-from-displaying-importerror-no-mo
+
+setup(console=['Vertaal_OefeningQT.py'])
